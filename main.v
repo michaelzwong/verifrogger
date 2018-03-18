742dd00f5a16c818d0375077bb2f5acc0e008eda
David Vajcenfeld <david.vajcenfeld@mail.utoronto.ca>
2018-03-18 18:35:38 -0400

    Added displaying of river objects.

--- main.v
+++ main.v
@@ -29,7 +29,8 @@ module main_test ();
         .clk(clk), .reset(reset),
 
         .draw_scrn_start(draw_scrn_start), .draw_scrn_game_over(draw_scrn_game_over),
-        .draw_scrn_game_bg(draw_scrn_game_bg), .draw_frog(draw_frog)
+        .draw_scrn_game_bg(draw_scrn_game_bg), .draw_frog(draw_frog),
+        .draw_river_obj_1(draw_river_obj_1), .draw_river_obj_2(draw_river_obj_2),
 
         .plot_done(plot_done),
 
@@ -42,7 +43,8 @@ module main_test ();
         .go(go), .plot_done(plot_done),
 
         .draw_scrn_start(draw_scrn_start), .draw_scrn_game_over(draw_scrn_game_over),
-        .draw_scrn_game_bg(draw_scrn_game_bg), .draw_frog(draw_frog)
+        .draw_scrn_game_bg(draw_scrn_game_bg), .draw_frog(draw_frog),
+        .draw_river_obj_1(draw_river_obj_1), .draw_river_obj_2(draw_river_obj_2)
     );
 
 endmodule // main_test 
@@ -56,7 +58,8 @@ endmodule // top
 module datapath (
     clk, reset,
 
-    draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg, draw_frog
+    draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg, draw_frog,
+    draw_river_obj_1, draw_river_obj_2,
 
     plot_done,
 
@@ -68,6 +71,7 @@ module datapath (
     input clk, reset;
 
     input draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg, draw_frog;
+    input draw_river_obj_1, draw_river_obj_2;
 
     output plot_done;
 
@@ -80,7 +84,8 @@ module datapath (
     output reg [8:0] y;
 
     wire draw;
-    assign draw = draw_scrn_start || draw_scrn_game_over || draw_scrn_game_bg || draw_frog;
+    assign draw = draw_scrn_start || draw_scrn_game_over || draw_scrn_game_bg || draw_frog
+                  || draw_river_obj_1 || draw_river_obj_2;
 
     // ### Timing adjustments. ###
 
@@ -88,8 +93,16 @@ module datapath (
         // Plot signal, x and y need to be delayed by one clock cycle
         // due to delay of retrieving data from memory.
         plot <= draw;
-        x <= next_x;
-        y <= next_y;
+        if (draw_river_obj_1) begin
+            x <= 20 + next_x;
+            y <= 20 + next_y;
+        end else if (draw_river_obj_2) begin
+            x <= 120 + next_x;
+            y <= 30 + next_y;
+        end else begin
+            x <= next_x;
+            y <= next_y;
+        end
     end
 
     // ### Plotter. ###
@@ -99,7 +112,7 @@ module datapath (
         .WIDTH_Y(9),
         .MAX_X(10),
         .MAX_Y(6)
-    ) plt_scrn_start (
+    ) plt_scrn (
         .clk(clk), .en(draw),
         .x(next_x), .y(next_y),
         .done(plot_done)
@@ -169,6 +182,35 @@ module datapath (
         .color_out(frog_color)
     );
 
+
+    // ### River objects. ###
+
+    wire [2:0] river_obj_1_color, river_obj_2_color;
+
+    sprite_ram_module #(
+        .WIDTH_X(4),
+        .WIDTH_Y(3),
+        .RESOLUTION_X(10),
+        .RESOLUTION_Y(6),
+        .MIF_FILE("mif_files/test.mif")
+    ) srm_river_obj_1 ( 
+        .clk(clk),
+        .x(next_x), .y(next_y),
+        .color_out(river_obj_1_color)
+    );
+
+    sprite_ram_module #(
+        .WIDTH_X(4),
+        .WIDTH_Y(3),
+        .RESOLUTION_X(10),
+        .RESOLUTION_Y(6),
+        .MIF_FILE("mif_files/test.mif")
+    ) srm_river_obj_2 ( 
+        .clk(clk),
+        .x(next_x), .y(next_y),
+        .color_out(river_obj_2_color)
+    );
+
     // ### Color mux. ###
 
     always @ (*) begin
@@ -181,6 +223,10 @@ module datapath (
             color = scrn_game_bg_color;
         else if (draw_frog)
             color = frog_color;
+        else if (draw_river_obj_1)
+            color = river_obj_1_color;
+        else if (draw_river_obj_2)
+            color = river_obj_2_color;
         else 
             color = 0;
     end
@@ -191,13 +237,15 @@ module control (
     clk, reset,
     go, plot_done,
 
-    draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg
+    draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg, draw_frog,
+    draw_river_obj_1, draw_river_obj_2
 );
 
     input clk, reset;
     input go, plot_done;
 
-    output reg draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg;
+    output reg draw_scrn_start, draw_scrn_game_over, draw_scrn_game_bg, draw_frog;
+    output reg draw_river_obj_1, draw_river_obj_2;
 
     reg [2:0] current_state, next_state;
 
@@ -207,7 +255,10 @@ module control (
                 S_WAIT_GAME_OVER        = 2,    // Wait before drawing GAME OVER screen.
                 S_DRAW_SCRN_GAME_OVER   = 3,    // Draw GAME OVER screen.
                 S_WAIT_GAME_BG          = 4,    // Wait before drawing game background.
-                S_DRAW_GAME_BG          = 5;    // Draw game background.
+                S_DRAW_GAME_BG          = 5,    // Draw game background.
+                S_WAIT_RIVER_OBJ        = 6,    // Wait before drawing river objects.
+                S_DRAW_RIVER_OBJ_1      = 7,    // Draw river object 1.
+                S_DRAW_RIVER_OBJ_2      = 8;    // Draw river object 2.
                 // Add frog movement states here?
 
     // State table.
@@ -224,7 +275,13 @@ module control (
             S_WAIT_GAME_BG:
                 next_state = go ? S_DRAW_GAME_BG : S_WAIT_GAME_BG;
             S_DRAW_GAME_BG:
-                next_state = plot_done ? S_WAIT_START : S_DRAW_GAME_BG;
+                next_state = plot_done ? S_WAIT_RIVER_OBJ : S_DRAW_GAME_BG;
+            S_WAIT_RIVER_OBJ:
+                next_state = go ? S_DRAW_RIVER_OBJ_1 : S_WAIT_RIVER_OBJ;
+            S_DRAW_RIVER_OBJ_1:
+                next_state = plot_done ? S_DRAW_RIVER_OBJ_2 : S_DRAW_RIVER_OBJ_1;
+            S_DRAW_RIVER_OBJ_2:
+                next_state = plot_done ? S_WAIT_START : S_DRAW_RIVER_OBJ_2;
         endcase
     end
 
@@ -242,6 +299,9 @@ module control (
         draw_scrn_start = 0;
         draw_scrn_game_over = 0;
         draw_scrn_game_bg = 0;
+        draw_river_obj_1 = 0;
+        draw_river_obj_2 = 0;
+        draw_frog = 0;
 
         // Set control signals based on state.
         case (current_state) 
@@ -253,6 +313,12 @@ module control (
             end
             S_DRAW_GAME_BG: begin
                 draw_scrn_game_bg = 1;
+            end
+            S_DRAW_RIVER_OBJ_1: begin
+                draw_river_obj_1 = 1;
+            end
+            S_DRAW_RIVER_OBJ_2: begin
+                draw_river_obj_2 = 1;
             end
         endcase
     end
--- sim_files/main.do
+++ sim_files/main.do
@@ -5,7 +5,7 @@ vsim -L altera_mf_ver -L altera_mf main_test
 
 # Add the signals to waveform.
 add wave -bin clk -bin go
-add wave -bin draw_scrn_start -bin draw_scrn_game_over
+add wave -bin /d0/draw
 add wave -bin plot -bin plot_done
 add wave -uns x -uns y -uns color
 
@@ -13,9 +13,8 @@ add wave -uns /c0/current_state
 
 # Force some possible inputs.
 force clk 0 0ns, 1 1ns -repeat 2ns
-force VGA_CLK 0 0ns, 1 2ns -repeat 4ns
 force reset 1 0ns, 0 4ns
-force go 0 0ns, 1 10ns, 0 15ns, 1 140ns, 0 145ns, 1 300ns, 0 305ns
+force go 0 0ns, 1 10ns, 0 15ns, 1 140ns, 0 145ns, 1 300ns, 0 305ns, 1 450ns, 0 455ns
 
 # Run simulation and zoom into relevant area of waveform.
 run 1000ns
