7694dbb2590501683c610190faffd3b6c3e8dc67
David Vajcenfeld <david.vajcenfeld@mail.utoronto.ca>
2018-03-17 22:37:52 -0400

    Added plotter module.

--- vga.v
+++ vga.v
@@ -1,6 +1,7 @@
 module vga_test ();
     
     wire clk;
+    wire plot_en, plot_done;
     wire [3:0] x;
     wire [2:0] y;
     wire [2:0] color;
@@ -15,6 +16,17 @@ module vga_test ();
         .clk(clk),
         .x(x), .y(y),
         .color_out(color)
+    );
+
+    plotter #(
+        .WIDTH_X(4),
+        .WIDTH_Y(3),
+        .MAX_X(10),
+        .MAX_Y(6)
+    ) p0 (
+        .clk(clk), .en(plot_en),
+        .x(x), .y(y),
+        .done(plot_done)
     );
 
 endmodule // vga_test 
@@ -56,4 +68,52 @@ module sprite_ram_module (
     
 
 endmodule // sprite_ram_module
+
+module plotter (
+    clk, en,
+    x, y,
+    done
+);
+
+    parameter WIDTH_X = 1;
+    parameter WIDTH_Y = 1;
+
+    parameter MAX_X = 1;
+    parameter MAX_Y = 1;
+
+    input clk, en;
+
+    output reg [WIDTH_X - 1:0] x;
+    output reg [WIDTH_Y - 1:0] y;
+    
+    output reg done;
+
+    always @ (posedge clk) begin
+        
+        // Increment on enable signal.
+        if (en) begin
+            if (x <= MAX_X) begin
+                x <= x + 1;
+            end else begin
+                x <= 0;
+                y <= y + 1;
+            end
+        end
+
+        // Reset when no enable signal.
+        if (!en) begin
+             x <= 0;
+             y <= 0;
+        end
+
+        // Signal done when reached max.
+        if (x == MAX_X - 1 && y == MAX_Y - 1) begin
+            done <= 1;
+        end else begin
+            done <= 0;
+        end
+
+    end
+
+endmodule // plotter 
 
